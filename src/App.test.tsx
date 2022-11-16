3857edc4de560177e0aa3edc72080b917c9ce6eb
joshunrau <contact@joshuaunrau.com>
2022-11-15 20:38:42 -0500

    add identity-obj-proxy

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-import TestRenderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
 
 import App from './App';
 
 test('adds 1 + 2 to equal 3', () => {
+  render(<App />)
   // Step 1: Arrange - Setup the component that we want to test
   // Step 2: Act - Run the logic that should be tested (e.g., exec function)
   // Step 3: Assert - Compare execution results with expected results
